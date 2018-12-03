af7bcadb16393401d1bc11d1354071d7d8f9066c
wildlifehexagon <crossthebreeze@gmail.com>
2018-12-03 15:37:10 -0600

    feat: add EditOrder function

--- internal/repository/arangodb/arangodb.go
+++ internal/repository/arangodb/arangodb.go
@@ -31,6 +31,7 @@ func NewOrderRepo(connP *manager.ConnectParams, coll string) (repository.OrderRe
 	return ar, nil
 }
 
+// GetOrder retrieves stock order from database
 func (ar *arangorepository) GetOrder(id string) (*model.OrderDoc, error) {
 	m := &model.OrderDoc{}
 	r, err := ar.database.Get(orderGet)
@@ -47,6 +48,7 @@ func (ar *arangorepository) GetOrder(id string) (*model.OrderDoc, error) {
 	return m, nil
 }
 
+// AddOrder creates a new stock order
 func (ar *arangorepository) AddOrder(no *order.NewOrder) (*model.OrderDoc, error) {
 	m := &model.OrderDoc{}
 	attr := no.Data.Attributes
@@ -73,10 +75,47 @@ func (ar *arangorepository) AddOrder(no *order.NewOrder) (*model.OrderDoc, error
 	return m, nil
 }
 
+// EditOrder updates an existing order
 func (ar *arangorepository) EditOrder(uo *order.OrderUpdate) (*model.OrderDoc, error) {
-
+	m := &model.AnnoDoc{}
+	attr := uo.Data.Attributes
+	// check if order exists
+	r, err := ar.database.Get(orderGet)
+	if err != nil {
+		return m, err
+	}
+	if r.IsEmpty() {
+		m.NotFound = true
+		return m, nil
+	}
+	if err := r.Read(m); err != nil {
+		return m, err
+	}
+	// create order document
+	bindVars := map[string]interface(){
+		"@stock_order_collection": ar.sorder.Name(),
+		"courier":                 attr.Courier,
+		"courier_account":         attr.CourierAccount,
+		"comments":                attr.Comments,
+		"payment":                 attr.Payment,
+		"purchase_order_num":      attr.PurchaseOrderNum,
+		"status":                  attr.Status,
+		"consumer":                attr.Consumer,
+		"payer":                   attr.Payer,
+		"purchaser":               attr.Purchaser,
+		"items":                   attr.Items,
+	}
+	rupd, err := ar.database.DoRun(orderIns, bindVars)
+	if err != nil {
+		return m, err
+	}
+	if err := rupd.Read(m); err != nil {
+		return m, err
+	}
+	return m, nil
 }
 
+// ListOrders provides a list of all orders
 func (ar *arangorepository) ListOrders(cursor int64, limit int64) ([]*model.OrderDoc, error) {
 
 }
