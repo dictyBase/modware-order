87267c148873007591854d1b3f22fe0d7b05a97b
wildlifehexagon <crossthebreeze@gmail.com>
2018-12-07 14:33:32 -0600

    test: convert unit test setup to use testarango library

--- internal/repository/arangodb/arangodb_test.go
+++ internal/repository/arangodb/arangodb_test.go
@@ -1,89 +1,65 @@
 package arangodb
 
 import (
-	"context"
 	"log"
 	"os"
-	"strconv"
 	"testing"
 
-	driver "github.com/arangodb/go-driver"
-	"github.com/dictyBase/apihelpers/aphdocker"
 	manager "github.com/dictyBase/arangomanager"
+	"github.com/dictyBase/arangomanager/testarango"
 	"github.com/dictyBase/go-genproto/dictybaseapis/order"
 )
 
-var ahost, aport, auser, apass, adb string
-var coll driver.Collection
-var collection = "stock_order"
+var ta *testarango.TestArango
 
-func newOrder(id string) *order.NewOrderAttributes {
-	return &order.NewOrderAttributes{
-		Courier:  "FedEx",
-		Comments: "This is a test comment",
-		Payment:  "Credit card",
+func getConnectParams() *manager.ConnectParams {
+	return &manager.ConnectParams{
+		User:     ta.User,
+		Pass:     ta.Pass,
+		Database: ta.Database,
+		Host:     ta.Host,
+		Port:     ta.Port,
+		Istls:    false,
+	}
+}
+
+func newOrder() *order.NewOrder {
+	return &order.NewOrder{
+		Data: &order.NewOrder_Data{
+			Type: "order",
+			Attributes: &order.NewOrderAttributes{
+				Courier:  "FedEx",
+				Comments: "This is a test comment",
+				Payment:  "Credit card",
+			},
+		},
 	}
 }
 
 func TestMain(m *testing.M) {
-	if len(os.Getenv("DOCKER_HOST")) == 0 {
-		os.Setenv("DOCKER_HOST", "unix:///var/run/docker.sock")
-	}
-	if len(os.Getenv("DOCKER_API_VERSION")) == 0 {
-		os.Setenv("DOCKER_API_VERSION", "1.35")
-	}
-
-	adocker, err := aphdocker.NewArangoDockerWithImage("arangodb:3.3.19")
-	adocker.Debug = true
+	ta, err := testarango.NewTestArangoFromEnv(true)
 	if err != nil {
-		log.Fatalf("Could not connect to docker: %s", err)
+		log.Fatal(err)
 	}
-	aresource, err := adocker.Run()
-	if err != nil {
-		log.Fatalf("Could not start resource: %s", err)
-	}
-	client, err := adocker.RetryConnection()
-	if err != nil {
-		log.Fatalf("unable to get client connection %s", err)
-	}
-	adb = aphdocker.RandString(6)
-	dbh, err := client.CreateDatabase(context.Background(), adb, &driver.CreateDatabaseOptions{})
-	if err != nil {
-		log.Fatalf("could not create arangodb database %s %s\n", adb, err)
-	}
-
-	coll, err = dbh.CreateCollection(context.Background(), collection, &driver.CreateCollectionOptions{})
-	if err != nil {
-		log.Fatalf("could not create arangodb collection %s", collection)
-	}
-	auser = adocker.GetUser()
-	apass = adocker.GetPassword()
-	ahost = adocker.GetIP()
-	aport = adocker.GetPort()
+	// clean up the database at the end
+	defer func() {
+		dbh, err := ta.DB(ta.Database)
+		if err != nil {
+			log.Fatal(err)
+		}
+		dbh.Drop()
+	}()
 	code := m.Run()
-	if err = adocker.Purge(aresource); err != nil {
-		log.Fatalf("unable to remove arangodb container %s\n", err)
-	}
 	os.Exit(code)
 }
 
-func TestGetOrder(t *testing.T) {
-	// convert port string to int
-	port, _ := strconv.Atoi(aport)
-
-	connP := &manager.ConnectParams{
-		User:     auser,
-		Pass:     apass,
-		Database: adb,
-		Host:     ahost,
-		Port:     port,
-		Istls:    false,
-	}
-	_, err := NewOrderRepo(connP, collection)
-	if err != nil {
-		t.Fatalf("error in connecting to data source %s", err)
-	}
-}
+// func TestGetOrder(t *testing.T) {
+// 	connP := getConnectParams()
+// 	_, err := NewOrderRepo(connP, "stock_order")
+// 	if err != nil {
+// 		t.Fatalf("error in connecting to data source %s", err)
+// 	}
+// }
 
 // func TestAddOrder(t *testing.T) {
 
