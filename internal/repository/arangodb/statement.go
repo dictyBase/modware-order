cfe2d2a0d4a0279de5a99f2b98e5d4d0d0b58b39
wildlifehexagon <crossthebreeze@gmail.com>
2018-12-12 12:50:30 -0600

    fix: use correct operator for cursor filtering

--- internal/repository/arangodb/statement.go
+++ internal/repository/arangodb/statement.go
@@ -35,7 +35,7 @@ const (
 	`
 	orderListWithCursor = `
 		FOR sorder in @@stock_order_collection
-			FILTER sorder.created_at < DATE_ISO8601(@next_cursor)
+			FILTER sorder.created_at <= DATE_ISO8601(@next_cursor)
 			SORT sorder.created_at DESC
 			LIMIT @limit
 			RETURN sorder
