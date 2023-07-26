2d969898e3481a60cc00125835cb7331c82fb0b2
Siddhartha Basu <biosidd@gmail.com>
2023-07-26 08:13:06 -0500

    fix(server.go): change parameter name from 'c' to 'clt' in RunServer function for better readability
    The parameter name in the RunServer function has been changed from 'c'
    to 'clt' to improve readability and make it clear that it is a
    cli.Context object. The variable name 'arPort' has been changed to
    'arangoPort' to improve semantics and make it clear that it represents
    the port for the ArangoDB connection. Similarly, 'anrepo' has been
    changed to 'arangoRepo' to improve semantics and make it clear that it
    represents the ArangoDB repository. 'ms' has been changed to
    'messagingServer' and 'msn' to 'messagingPublisher' to improve semantics
    and make it clear that they represent the messaging server and publisher
    respectively. 'grpcS' has been changed to 'grpcServer' to improve
    semantics and make it clear that it represents the gRPC server. 'endP'
    has been changed to 'endpoint' to improve semantics and make it clear
    that it represents the server endpoint. In the getLogger function, the
    parameter name has been changed from 'c' to 'clt' for better
    readability. The variable name 'l' has been changed to 'logLevel' to
    improve semantics and make it clear that it represents the log level.

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -24,76 +24,72 @@ import (
 	"google.golang.org/grpc/reflection"
 )
 
-// RunServer starts and runs the server
-func RunServer(c *cli.Context) error {
-	arPort, _ := strconv.Atoi(c.String("arangodb-port"))
+// RunServer starts and runs the server.
+func RunServer(clt *cli.Context) error {
+	arPort, _ := strconv.Atoi(clt.String("arangodb-port"))
 	connP := &manager.ConnectParams{
-		User:     c.String("arangodb-user"),
-		Pass:     c.String("arangodb-pass"),
-		Database: c.String("arangodb-database"),
-		Host:     c.String("arangodb-host"),
+		User:     clt.String("arangodb-user"),
+		Pass:     clt.String("arangodb-pass"),
+		Database: clt.String("arangodb-database"),
+		Host:     clt.String("arangodb-host"),
 		Port:     arPort,
-		Istls:    c.Bool("is-secure"),
+		Istls:    clt.Bool("is-secure"),
 	}
 	anrepo, err := arangodb.NewOrderRepo(connP, "stock_order")
 	if err != nil {
 		return cli.NewExitError(
-			fmt.Sprintf("cannot connect to arangodb order repository %s", err.Error()),
-			2,
-		)
+			fmt.Sprintf("cannot connect to arangodb order repository %s",
+				err.Error(),
+			), 2)
 	}
-	ms, err := nats.NewPublisher(
-		c.String("nats-host"),
-		c.String("nats-port"),
+	msn, err := nats.NewPublisher(
+		clt.String("nats-host"),
+		clt.String("nats-port"),
 		gnats.MaxReconnects(-1),
 		gnats.ReconnectWait(2*time.Second),
 	)
 	if err != nil {
 		return cli.NewExitError(
-			fmt.Sprintf("cannot connect to messaging server %s", err.Error()),
-			2,
-		)
+			fmt.Sprintf("cannot connect to messaging server %s",
+				err.Error(),
+			), 2)
 	}
 	grpcS := grpc.NewServer(
 		grpc_middleware.WithUnaryServerChain(
 			grpc_ctxtags.UnaryServerInterceptor(),
-			grpc_logrus.UnaryServerInterceptor(getLogger(c)),
+			grpc_logrus.UnaryServerInterceptor(getLogger(clt)),
 		),
 	)
 	order.RegisterOrderServiceServer(
-		grpcS,
-		service.NewOrderService(
-			anrepo,
-			ms,
-			aphgrpc.TopicsOption(
-				map[string]string{
-					"orderCreate": "OrderService.Create",
-					"orderUpdate": "OrderService.Update",
-				}),
+		grpcS, service.NewOrderService(anrepo, msn,
+			aphgrpc.TopicsOption(map[string]string{
+				"orderCreate": "OrderService.Create",
+				"orderUpdate": "OrderService.Update",
+			}),
 		),
 	)
-	if c.Bool("reflection") {
+	if clt.Bool("reflection") {
 		// register reflection service on gRPC server
 		reflection.Register(grpcS)
 	}
 	// create listener
-	endP := fmt.Sprintf(":%s", c.String("port"))
+	endP := fmt.Sprintf(":%s", clt.String("port"))
 	lis, err := net.Listen("tcp", endP)
 	if err != nil {
-		return cli.NewExitError(
-			fmt.Sprintf("failed to listen %s", err),
-			2,
-		)
+		return cli.NewExitError(fmt.Sprintf("failed to listen %s", err), 2)
 	}
 	log.Printf("starting grpc server on %s", endP)
-	grpcS.Serve(lis)
+	if err := grpcS.Serve(lis); err != nil {
+		return fmt.Errorf("error in starting grpc server %s", err)
+	}
+
 	return nil
 }
 
-func getLogger(c *cli.Context) *logrus.Entry {
+func getLogger(clt *cli.Context) *logrus.Entry {
 	log := logrus.New()
 	log.Out = os.Stderr
-	switch c.GlobalString("log-format") {
+	switch clt.GlobalString("log-format") {
 	case "text":
 		log.Formatter = &logrus.TextFormatter{
 			TimestampFormat: "02/Jan/2006:15:04:05",
@@ -103,7 +99,7 @@ func getLogger(c *cli.Context) *logrus.Entry {
 			TimestampFormat: "02/Jan/2006:15:04:05",
 		}
 	}
-	l := c.GlobalString("log-level")
+	l := clt.GlobalString("log-level")
 	switch l {
 	case "debug":
 		log.Level = logrus.DebugLevel
@@ -116,5 +112,6 @@ func getLogger(c *cli.Context) *logrus.Entry {
 	case "panic":
 		log.Level = logrus.PanicLevel
 	}
+
 	return logrus.NewEntry(log)
 }
