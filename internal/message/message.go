ea479b58ac832556b5974403e297b8b0efe27a86
Siddhartha Basu <biosidd@gmail.com>
2023-07-26 18:30:01 -0500

    chore(message.go): add period at the end of the comment for consistency
    The changes in message.go and repository.go files are purely cosmetic.
    They add a period at the end of the comments for consistency and
    improved readability.

--- internal/message/message.go
+++ internal/message/message.go
@@ -4,7 +4,7 @@ import (
 	"github.com/dictyBase/go-genproto/dictybaseapis/order"
 )
 
-// Publisher manages publishing of message
+// Publisher manages publishing of message.
 type Publisher interface {
 	// Publish publishes the order object using the given subject
 	Publish(subject string, ord *order.Order) error
